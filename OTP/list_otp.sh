558d1bc4516dc3b00db0e24dc0f9dd83e33de480
Andy Rosen <ajr@corp.mlfs.org>
2020-11-09 09:47:32 -0500

    Remove the "Add new MFA device" option once an argument is typed

--- OTP/list_otp.sh
+++ OTP/list_otp.sh
@@ -10,8 +10,11 @@ else
     if [ $? != 0 ]; then
 	echo '{ "title": "Create new OTP database", "arg": "new", "subtitle": "'${db}'" }'
     else
-	echo '{ "title": "Add new MFA device", "arg": "add", "subtitle": "'${db}'" }'
-	sqlite3 ${db} 'select alfred from totp order by alfred;'
+	echo '{}'
+	if [ "${1}" == "" ]; then
+	    echo ',{ "title": "Add new MFA device", "arg": "add", "subtitle": "'${db}'" }'
+	fi
+	sqlite3 ${db} 'select alfred from totp order by alfred;' | grep -i '"title": "[^"]*'${1}'[^"]*"'
     fi
 fi
 
