8a5f411c920ed3173a4772df2d63dd84c4d19a56
Andy Rosen <ajr@corp.mlfs.org>
2021-05-18 12:12:38 -0400

    Show item using a tmp file instead of the clipboard

--- LastPass Accelerator/show_item.sh
+++ LastPass Accelerator/show_item.sh
@@ -2,32 +2,37 @@
 
 export LPASS_DISABLE_PINENTRY=1
 
+cd "${alfred_workflow_cache}"
+
+trap "rm -f pb" EXIT
+
 icondir="${alfred_preferences}/workflows/${alfred_workflow_uid}"
 icon="${icondir}/icon.png"
 
 # Get the item
-pb=$(echo "${lppass}" | 2>&- ${lppath} show ${lpitem})
+item=$(echo "${lppass}" | 2>&- ${lppath} show ${lpitem})
 
 while [ $? != 0 ]; do
     lppass=$(${alfred_preferences}/workflows/${alfred_workflow_uid}/reprompt.applescript)
     [ "${lppass}" == "" ] && exit
-    pb=$(echo "${lppass}" | 2>&- ${lppath} show ${lpitem})
+    item=$(echo "${lppass}" | 2>&- ${lppath} show ${lpitem})
 done
 
-# Copy it to the clipboard
-echo "${pb}" | grep -v "\[id: ${lpitem}]$" | pbcopy
+# echo "${item}" | grep -v "\[id: ${lpitem}]$" > pb
+echo "${item}" > pb
 
 # Look for an icon that matches the note type
-type=$(pbpaste | grep '^NoteType: ' | sed 's/^NoteType: //')
+type=$(grep '^NoteType: ' pb | sed 's/^NoteType: //')
 if [ -f "${icondir}/sn/${type}.png" ]; then icon="${icondir}/sn/${type}.png"; fi
 
 # Look for an icon that matches the credit card type
 if [ "${type}" == "Credit Card" ]; then
-    type=$(pbpaste | grep '^Type: ' | sed 's/^Type: //')
+    type=$(grep '^Type: ' pb | sed 's/^Type: //')
     if [ -f "${icondir}/sn/${type}.png" ]; then icon="${icondir}/sn/${type}.png"; fi
 fi
 
 osascript -e 'tell application "System Events"' \
+	  -e 'set t to (do shell script "cat pb")' \
 	  -e 'set i to "'"${icon}"'"' \
-	  -e 'display dialog (the clipboard) buttons {"OK"} default button "OK" with title "LastPass Accelerator" with icon posix file i' \
+	  -e 'display dialog t buttons {"OK"} default button "OK" with title "LastPass Accelerator" with icon posix file i' \
 	  -e 'end tell'
